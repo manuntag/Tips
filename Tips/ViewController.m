b4ff763dd758780c11c0659ae01e09337e21c54b
manuntag <manuntag@ualberta.ca>
2015-01-23 13:00:25 -0800

    Initial Commit

---
+++ Tips/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tips
+//
+//  Created by David Manuntag on 2015-01-23.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
